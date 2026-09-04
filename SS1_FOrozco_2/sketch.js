04b5baf4a29cfe56c9f9a6e9958c00b31b60286d
FOrozco125 <forozco125@gmail.com>
2026-09-04 13:14:25 -0400

    SS1 Draft
    First draft w/ shapes

--- SS1_FOrozco_2/sketch.js
+++ SS1_FOrozco_2/sketch.js
@@ -1,14 +1,18 @@
 /*
 Name:  Fernando Orozco
-Title:
+Title:  Basic Shapes with Code
 */
 
 function setup() {
-  createCanvas(700, 500);
+  createCanvas(700, 600);
 }
 
 function draw() {
   background("blue");
 
 rect(50,50,200,200)
+
+square(350,50,200,200)
+
+triangle(350, 300, 150, 500, 550, 500)
 }
