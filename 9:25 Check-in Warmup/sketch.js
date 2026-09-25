76d471a05e356e57cf509502199ddcf89871ed86
FOrozco125 <forozco125@gmail.com>
2026-09-25 10:30:44 -0400

    Warmup_Week4

---
+++ 9:25 Check-in Warmup/sketch.js
@@ -0,0 +1,17 @@
+/*
+Name:  Fernando O
+Title:  Warmup_Week4
+*/
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(200);
+
+  // Draw a vertical line that follows the mouse's x-coordinate.
+  line(mouseX, 0, mouseX, 500);
+}
+
+
